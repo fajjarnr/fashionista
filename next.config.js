a195757e263c5bd1bcbfcee3b16d38e6323e9463
fajjarnr <zfajart@gmail.com>
2022-01-15 11:11:16 +0700

    first commit

--- next.config.js
+++ next.config.js
@@ -1,3 +1,11 @@
 module.exports = {
   reactStrictMode: true,
-}
+  env: {
+    SHOPIFY_STORE_DOMAIN: process.env.SHOPIFY_STORE_DOMAIN,
+    SHOPIFY_STOREFRONT_ACCESS_TOKEN:
+      process.env.SHOPIFY_STOREFRONT_ACCESS_TOKEN,
+  },
+  images: {
+    domains: ['cdn.shopify.com'],
+  },
+};
